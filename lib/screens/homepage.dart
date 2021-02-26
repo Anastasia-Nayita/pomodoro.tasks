e4efcee3b48fba72f8d001ba8eaf083ceeee4665
Anastasia-Nayita <kekelidou@gmail.com>
2021-02-26 23:56:55 +0100

    homepage added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,25 +10,52 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Stack(
-        children: [
-          Image(
-            image: AssetImage(
-              'assets/images/blue.png',
-            ),
-          ),
-          Align(
-            alignment: Alignment(0.0, -0.5),
-            child: Image(
+      body: Container(
+        child: Stack(
+          children: [
+            Image(
               image: AssetImage(
-                'assets/images/todoList.png',
+                'assets/images/blue.png',
               ),
             ),
-          ),
-          TaskCardWidget(),
-        ],
+            Align(
+              alignment: Alignment(0.0, -0.5),
+              child: Image(
+                image: AssetImage(
+                  'assets/images/todoList.png',
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 165,
+              right: 20,
+              left: 20,
+              child: TaskCardWidget(),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.8),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 100.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xFFC9B07F),
+                ),
+                child: Text(
+                  'Get Started!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,13 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class TaskCardWidget extends StatelessWidget {
+  final String title;
+  final String desc;
+  TaskCardWidget({this.title, this.desc});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment(0.0, 0.3),
-      child: Text(
-        "Getting things done with Todocious",
-        style: TextStyle(color: Colors.white),
+      padding: EdgeInsets.symmetric(
+        vertical: 25.0,
+        horizontal: 20.0,
+      ),
+      margin: EdgeInsets.only(
+        bottom: 8.0,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.deepPurple,
+          width: 4,
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+            ),
+            child: Text(
+              title ?? "Getting things done with Todocious",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15.0,
+            ),
+            child: Text(
+                desc ??
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae, a velit facilisis aliquet suscipit.",
+                style: TextStyle(color: Colors.white, fontSize: 13.0),
+                textAlign: TextAlign.center),
+          ),
+        ],
       ),
     );
   }
